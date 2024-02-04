e3ac3b488f495fcc63e5e456c12d144c98b9da70
wesjiii <153965793+wesjiii@users.noreply.github.com>
2024-02-03 17:43:42 -0800

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,7 @@
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+var Sent3 = "\"This is the beginning of the string" + " and this is the end of the string\""; //added purely concatenated sentence
+document.write(Sent1 + Sent2 + "<br>"); //Concatenates the Sent1 and Sent2 variables. 
+//Also an expression. Added a break to separate the following expression
+document.write(20 + 20 + "<br>"); //writing a pure expression and adding a break
+document.write(Sent3); // telling Sent3 to show up on screen
