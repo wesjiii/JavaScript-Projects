adc049137a3b8ce5a0152638c111ba734c8e93aa
wesjiii <153965793+wesjiii@users.noreply.github.com>
2024-02-06 23:19:31 -0800

    Project2_functions corrected
    Uploaded my re-edited version of index.html and reorganized Project2_functions folder

---
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var Concatenate = "I am learning";
+    var sentence = " a lot from this book";
+    document.getElementById("myFunction").innerHTML = ("I am learning" + " a lot from this book");
+}
