025209248c12fca547a7289d2902792442a3f136
wesjiii <153965793+wesjiii@users.noreply.github.com>
2024-02-10 16:04:04 -0800

    Update main.js

--- Basic JavaScript Projects/Project4_dictionaries/JS/main.js
+++ Basic JavaScript Projects/Project4_dictionaries/JS/main.js
@@ -1,10 +1,10 @@
-function my_Animals() {
-    var Animal = {
-        cats: 12,
-        dogs: 10,
-        fish: 2,
-        rabbits: 15
+function my_Animals() { //assigning name or label to the function
+    var Animal = { //assigning variable name
+        cats: 12, // assigning a value of 12 to the label cats
+        dogs: 10, // assigning a value of 10 to the label dogs
+        fish: 2, // assigning a value of 2 to the label fish
+        rabbits: 15 // assigning a value of 15 to the label rabbits
     };
-    delete dogs.fish;
-    document.getElementById("one").innerHTML = dogs.fish;
+    delete Animal.fish; //deleting the varable fish
+    document.getElementById("one").innerHTML = Animal.cats; //telling the function to provide the value for cats once the user interacts with all lines affected by the "one" ID
 }
