2239159d0bdfdbe58100ebb936190e5edbeb730d
wesjiii <153965793+wesjiii@users.noreply.github.com>
2024-03-10 22:21:02 -0700

    JavaScript Folder with Calculator application
    adding JavaScript projects Folder with the calculator included

---
+++ Basic JavaScript Projects/JavaScript Projects/JavaScript/JavaScript_1.js
@@ -0,0 +1,8 @@
+const c = document.getElementById("myCanvas");
+const ctx = c.getContext("2d");
+const grd = ctx.createLinearGradient(0, 0, 170, 0);
+grd.addColorStop(0, "black");
+grd.addColorStop(1, "white");
+
+ctx.fillStyle = grd;
+ctx.fillRect(20, 20, 150, 100);
